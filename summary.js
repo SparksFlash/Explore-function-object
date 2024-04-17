86859f9fc6a95ffc681feb64eaceb14db2e85d68
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-04-17 12:55:19 +0600

    summary

--- summary.js
+++ summary.js
@@ -5,12 +5,12 @@ phones[3] = 'walton';
 
 // check oppo not exists in an array
 if (phones.indexOf('oppo') == -1) {
-    console.log('opps! amir khan oppo is missing');
+    console.log('opps! amir khan oppo is missing')
 }
 
 // if lg is available
 if (phones.indexOf('lg') != -1) {
-    console.log('Lg is available now');
+    console.log('Lg is available now')
 }
 
 // loop
@@ -18,22 +18,20 @@ var num = 0;
 while (num <= 10) {
     num++;
 }
-
-for (var i = 0; i < phones.length; i++) {
-
-}
+console.log(num)
 
 // function
-function addThreeNumbers(number1, number2, number3) {
+function addThreeNumbers(number1, number2, number3){
     var total = number1 + number2 + number3;
     return total;
 }
 
-addThreeNumbers(87, 41, 56);
+
+addThreeNumbers(10, 20, 30);
 
 // object
-var microphone = {
+/*var microphone = {
     brand: 'blue yeti',
     price: 120,
     color: black
-}
+}*/
